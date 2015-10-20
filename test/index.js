0a51a522a36b261aa90f34c74869651df3ae00a4
Dmitry Sky <dmitry.sky@icloud.com>
2015-10-20 09:37:29 -0600

    assert, assert.equal using chai testing

--- test/index.js
+++ test/index.js
@@ -5,4 +5,15 @@ describe('my test suite', function () {
   it('should work', function () {
     assert(true);
   });
+
+  it('should add two plus two', function (){
+    let four = 2 + 2;
+    assert.equal(four, 4);
+    // assert.strictEqual("4", 4); This will fail
+  })
+
+  it('should be deep equal', function (){
+    let x = [1,2,3];
+    // assert.deepEqual(x, [1,2,3]);
+  });
 });
